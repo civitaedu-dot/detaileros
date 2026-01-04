219c0ac7b1ca405a32f6ed257e4dc7e024c9cc54
civitaedu-dot <civitaedu@gmail.com>
2026-01-04 01:59:04 -0300

    commit fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <main style={{ padding: 40 }}>
+    <main>
       <h1>DetailerOS</h1>
       <p>SaaS para Estéticas Automotivas</p>
     </main>
   )
 }
+
 
 
 
